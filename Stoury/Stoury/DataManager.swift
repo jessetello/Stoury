e94d26bcfa8bfb965d1d004d7217f2be962bf850
jessetello <jesse.tello@gmail.com>
2017-02-23 21:07:11 -0500

    recent posts fetch

--- Stoury/Stoury/DataManager.swift
+++ Stoury/Stoury/DataManager.swift
@@ -14,40 +14,35 @@ import FirebaseAuth
 class DataManager {
     
     static let sharedInstance = DataManager()
-    let postRef = FIRDatabase.database().reference()
-    let userRef = FIRDatabase.database().reference()
-
+    
+    let postRef = FIRDatabase.database().reference(withPath: "posts")
+    let userRef = FIRDatabase.database().reference(withPath: "user-posts")
     let storage = FIRStorage.storage()
+
     var recentPosts = [Stoury]()
     var userPosts = [Stoury]()
     
     typealias DataHandler = (_ success:Bool, _ data:[Stoury]) -> Void
 
     func getUserFeed(completion: @escaping DataHandler) {
-//        ref.child("user-posts")
-//        ref.observe(FIRDataEventType.value, with: { (snapshot) in
-//            if let posts = snapshot.value {
+//        userRef.observe(FIRDataEventType.value, with: { (snapshot) in
+//            if let userPosts = snapshot.value {
 //                print(posts)
-//               // completion(true, posts)
+//                completion(true, posts)
 //            }
 //        })
     }
     
     func getRecentPosts(completion: @escaping DataHandler) {
         //search database all posts for most recent
-        postRef.child("posts")
         postRef.observe(FIRDataEventType.value, with: { (snapshot) in
-            let dict = snapshot.value as? [String : AnyObject] ?? [:]
-            if let posts = dict["posts"] as? [String : [String : Any]] {
-                print(posts)
+            if let posts = snapshot.value as? [String : [String : Any]] {
                 for (key, value) in posts {
                     let info = value as [String: Any]
                     let stoury = Stoury(userID: key, userName: info[""] as? String, title:  info[""] as? String, location:  info[""] as? String, length:  info[""] as? Double, date:  info[""] as? Date, category:  info[""] as? String)
                     self.recentPosts.append(stoury)
                 }
-                completion(true, self.recentPosts)
             }
-            completion(false, self.recentPosts)
         })
     }
     
